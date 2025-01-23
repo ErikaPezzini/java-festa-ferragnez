89307be67908525bd393676418ca532418866afb
ErikaPezzini <erikapezzini66@gmail.com>
2025-01-23 21:37:32 +0100

    feat: Check Guest con while

--- src/CheckGuestWhile.java
+++ src/CheckGuestWhile.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class CheckGuestWhile {
-    
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        // Array invitati
+        String [] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        // Chiedere il nome all'user
+        System.out.println("Inserisci nome e cognome:");
+        String guest = scan.nextLine();
+
+        // Verificare che il nome sia presente
+        boolean invited = false;
+        int i = 0;
+        while (i < guestList.length) {
+            if (guestList[i].equalsIgnoreCase(guest)) {
+                invited = true;
+                break;
+            }
+            i++;
+        }
+
+        // Lasciarlo entrare o no
+        if (invited) {
+            System.out.println("Benvenuto/a alla festa," + guest + "!");
+        } else {
+            System.out.println("Mi dispiace, " + guest + ", ma non sei nella lista degli invitati. Non puoi entrare!");
+        }
+    }
 }
